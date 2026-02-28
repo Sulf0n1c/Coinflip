67e7e7a482f9cd5d81fe35567cef5ecdb2393fc5
Sulf0n1c <alexswhitehurst69@gmail.com>
2026-02-27 22:35:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const publicDir = path.join(__dirname, "public");
 app.use(express.json());
 
 // CORS
