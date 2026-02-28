11b12b0013153b227f688de634dd8489305b01d6
Sulf0n1c <alexswhitehurst69@gmail.com>
2026-02-27 22:44:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const publicDir = path.join(__dirname, "public");
-app.use(express.json());
+app.use(express.static(publicDir));
 
 // CORS
 const ALLOWED_ORIGINS = [
